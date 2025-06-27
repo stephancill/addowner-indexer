78c9bb2ce94681fcc4fbad929b9bca0a456fbecd
Stephan Cilliers <stephan.cilliers@coinbase.com>
2025-06-27 17:01:17 +0200

    fix: update schema

--- ponder.schema.ts
+++ ponder.schema.ts
@@ -2,8 +2,8 @@ import { onchainTable } from "ponder";
 
 export const owners = onchainTable("owners", (t) => ({
   id: t.text().primaryKey(),
-  address: t.text(),
-  owner: t.text(),
+  address: t.hex(),
+  owner: t.hex(),
   index: t.integer(),
   createdAt: t.timestamp(),
   updatedAt: t.timestamp(),
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import { db } from "ponder:api";
 import schema from "ponder:schema";
 import { Hono } from "hono";
-import { client, graphql } from "ponder";
+import { client, eq, graphql, or } from "ponder";
 
 const app = new Hono();
 
@@ -9,5 +9,19 @@ app.use("/sql/*", client({ db, schema }));
 
 app.use("/", graphql({ db, schema }));
 app.use("/graphql", graphql({ db, schema }));
+
+app.get("/events/:address", async (c) => {
+  const { address } = c.req.param();
+  const events = await db
+    .select()
+    .from(schema.owners)
+    .where(
+      or(
+        eq(schema.owners.address, address as `0x${string}`),
+        eq(schema.owners.owner, address as `0x${string}`)
+      )
+    );
+  return c.json(events);
+});
 
 export default app;
